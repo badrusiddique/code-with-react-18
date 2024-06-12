4c34bc0d494a9af8187c5f134d12d39c2cd35385
Badru Siddique <123924787+badrusiddique@users.noreply.github.com>
2024-06-12 11:29:11 +0000

    alerts

--- vite-project/src/Message.tsx
+++ vite-project/src/Message.tsx
@@ -1,7 +1,35 @@
 import * as React from "react";
 
 function Message() {
-  return <h1 className="m-2">Welcome !!!</h1>;
+  const [showAlert, setShowAlert] = React.useState(false);
+
+  const handleClick = (value: boolean) => {
+    setShowAlert(value);
+  };
+
+  return (
+    <div>
+      <button className="btn btn-primary m-2" onClick={() => handleClick(true)}>
+        Click Me
+      </button>
+      {showAlert && (
+        <div
+          className="alert alert-success alert-dismissible fade show"
+          role="alert"
+        >
+          <strong>Holy guacamole!</strong> You should check in on some of those
+          fields below.
+          <button
+            type="button"
+            className="btn-close"
+            data-bs-dismiss="alert"
+            aria-label="Close"
+            onClick={() => handleClick(false)}
+          ></button>
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default Message;
